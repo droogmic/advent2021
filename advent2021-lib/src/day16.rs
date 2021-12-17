f4fc16930592ed5a954d5da6353d8c660aadbc64
Michael Droogleever Fortuyn <droogmic@gmail.com>
2021-12-17 20:29:18 +0100

    WIP

--- advent2021-lib/src/day16.rs
+++ advent2021-lib/src/day16.rs
@@ -62,6 +62,13 @@ impl std::convert::TryFrom<&PacketBits> for PacketLength {
     }
 }
 
+fn length_from_bits(bits: &PacketBits) -> Result<(PacketLength, &PacketBits), ParseError> {
+    Ok((bits.try_into()?, &match bits[0] {
+        false => bits[16..],
+        true => bits[12..],
+    }))
+}
+
 #[derive(Debug)]
 pub enum PacketData {
     Literal(usize),
@@ -74,7 +81,7 @@ pub struct Packet {
     data: PacketData,
 }
 
-fn recursive_parse(bits: &PacketBits) -> Result<(Packet, &PacketBits), ParseError> {
+fn recursive_parse(bits: &PacketBits) -> Result<(Packet, &PacketBits, usize), ParseError> {
     let Header {
         version,
         packet_type,
@@ -96,50 +103,39 @@ fn recursive_parse(bits: &PacketBits) -> Result<(Packet, &PacketBits), ParseErro
             PacketData::Literal(literal_bits.load::<usize>())
         }
         PacketType::Operator => {
-            // let length: PacketLength = bits.try_into()?;
-            let packets = vec![];
-            match bits[6..].try_into()? {
+            let (length, bits) = length_from_bits(bits)?;
+            let mut packets = vec![];
+            match length {
                 PacketLength::Bits(bit_count) => {
-                    let next_packet = recursive_parse()
-                    packets
+                    let mut bits_consumed = 0;
+                    let mut bits = bits;
+                    while bits_consumed < bit_count {
+                        let (next_packet, next_bits, bits_packet) = recursive_parse(bits)?;
+                        packets.push(next_packet);
+                        bits = next_bits;
+                        bits_consumed += bits_packet;
+                    }
                 },
-                PacketLength::Packets(packet_count) => unreachable!(),
+                PacketLength::Packets(packet_count) => {
+                    let mut bits = bits;
+                    while packets.len() < packet_count {
+                        let (next_packet, next_bits, _bits_packet) = recursive_parse(bits)?;
+                        packets.push(next_packet);
+                        bits = next_bits;
+                    }
+                },
             }
-            PacketData::Operator()
+            PacketData::Operator(packets)
         }
     };
-    Ok((Packet { version, data }, bits))
+    Ok((Packet { version, data }, bits, 0))
 }
 
 impl std::convert::TryFrom<&PacketBits> for Packet {
     type Error = ParseError;
     fn try_from(bits: &PacketBits) -> Result<Self, Self::Error> {
-        let Header {
-            version,
-            packet_type,
-        } = bits[0..6].try_into()?;
-        let data = match packet_type {
-            PacketType::Literal => {
-                // the binary number is padded with leading zeroes
-                // until its length is a multiple of four bits,
-                // and then it is broken into groups of four bits
-                let literal_bits: BitVec<Lsb0, usize> = bitvec![];
-                for group in bits[6..]
-                    .chunks_exact(5)
-                    // each group is prefixed by a 1 bit except the last group,
-                    // which is prefixed by a 0 bit
-                    .take_until(|literal_part| literal_part.first().unwrap() == false)
-                {
-                    literal_bits.extend_from_bitslice(&group[1..5])
-                }
-                PacketData::Literal(literal_bits.load::<usize>())
-            }
-            PacketType::Operator => {
-                let length: PacketLength = bits.try_into()?;
-                PacketData::Operator(vec![])
-            }
-        };
-        Ok(Packet { version, data })
+        let (packet, _, _) = recursive_parse(bits)?;
+        Ok(packet)
     }
 }
 
@@ -166,7 +162,7 @@ pub const DAY: Day<Packet, usize> = Day {
         part1,
         part2,
     },
-    example: include_str!("../examples/day00.txt"),
+    example: "", //include_str!("../examples/day00.txt"),
 };
 
 #[cfg(test)]
